af319355b022569258155526c9aa708f063fbd43
Samuel Mullen <samuel@samuelmullen.com>
2016-08-22 07:04:23 -0500

    Nix user from polls

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729013127) do
+ActiveRecord::Schema.define(version: 20160822120258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "polls", force: :cascade do |t|
-    t.integer  "user_id",     null: false
     t.string   "name",        null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
-    t.index ["user_id"], name: "index_polls_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
